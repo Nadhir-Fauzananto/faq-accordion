274e17535129b4a04144d7c5999be2d593c5d371
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-05-13 12:54:30 +0200

    add aria stuffs

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ faqContents.forEach(content => {
   const answer = content.querySelector('p');
 
   if (answer) {
-    plusIcon.addEventListener('click', () => {
+    if (!plusIcon || !minusIcon) {
+      console.error('Plus or minus icon not found for this FAQ item.');
+      return;
+    }
+
+    else {
+      plusIcon.addEventListener('click', () => {
       answer.classList.remove('is-hidden');
       answer.classList.add('is-visible');
       plusIcon.classList.remove('is-visible');
@@ -15,38 +21,39 @@ faqContents.forEach(content => {
       minusIcon.classList.add('is-visible');
     });
 
-    plusIcon.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter' || event.key === ' ') {
-        answer.classList.remove('is-hidden');
-        answer.classList.add('is-visible');
-        plusIcon.classList.remove('is-visible');
-        plusIcon.classList.add('is-hidden');
-        minusIcon.classList.remove('is-hidden');
-        minusIcon.classList.add('is-visible');
-      }
+      plusIcon.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+          answer.classList.remove('is-hidden');
+          answer.classList.add('is-visible');
+          plusIcon.classList.remove('is-visible');
+          plusIcon.classList.add('is-hidden');
+          minusIcon.classList.remove('is-hidden');
+          minusIcon.classList.add('is-visible');
+        }
     });
 
-    minusIcon.addEventListener('click', () => {
-      answer.classList.remove('is-visible');
-      answer.classList.add('is-hidden');
-      plusIcon.classList.remove('is-hidden');
-      plusIcon.classList.add('is-visible');
-      minusIcon.classList.remove('is-visible');
-      minusIcon.classList.add('is-hidden');
-    });
-
-    minusIcon.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter' || event.key === ' ') {
+      minusIcon.addEventListener('click', () => {
         answer.classList.remove('is-visible');
         answer.classList.add('is-hidden');
         plusIcon.classList.remove('is-hidden');
         plusIcon.classList.add('is-visible');
         minusIcon.classList.remove('is-visible');
         minusIcon.classList.add('is-hidden');
-      }
     });
+
+      minusIcon.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+          answer.classList.remove('is-visible');
+          answer.classList.add('is-hidden');
+          plusIcon.classList.remove('is-hidden');
+          plusIcon.classList.add('is-visible');
+          minusIcon.classList.remove('is-visible');
+          minusIcon.classList.add('is-hidden');
+        }
+    });
+    }
   } 
   else {
-    console.error('No answer found for this FAQ item.');
+    console.error('Answer not found for this FAQ item.');
   }
 });
