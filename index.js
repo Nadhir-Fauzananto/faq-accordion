3c8662b5fb440a244c06c837131805f2339fc822
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-05-13 12:15:06 +0200

    update error handling

--- index.js
+++ index.js
@@ -8,40 +8,65 @@ faqContents.forEach(content => {
   if (answer) {
         if (plusIcon) {
             plusIcon.addEventListener('click', () => {
-            answer.classList.add('is-visible');
-            answer.classList.remove('is-hidden');
-            plusIcon.classList.add('is-hidden');
-            minusIcon.classList.remove('is-hidden');
-        });
-
-            plusIcon.addEventListener('keydown', (event) => {
-                if(event.key === 'Enter' || event.key === 'Space') {
+                if(minusIcon) {
                     answer.classList.add('is-visible');
                     answer.classList.remove('is-hidden');
                     plusIcon.classList.add('is-hidden');
                     minusIcon.classList.remove('is-hidden');
                 }
+                else {
+                    console.error("Minus icon not found.");
+                }
         });
-    }
-
-    if (minusIcon) {
-            minusIcon.addEventListener('click', () => {
-            answer.classList.remove('is-visible');
-            answer.classList.add('is-hidden');
-            plusIcon.classList.remove('is-hidden');
-            minusIcon.classList.add('is-hidden');
-        });
-
-            minusIcon.addEventListener('keydown', (event) => {
-                if(event.key === 'Enter' || event.key === 'Space') {
-                    answer.classList.remove('is-visible');
-                    answer.classList.add('is-hidden');
-                    plusIcon.classList.remove('is-hidden');
-                    minusIcon.classList.add('is-hidden');
+            plusIcon.addEventListener('keydown', (event) => {
+                if(minusIcon) {
+                    if(event.key === 'Enter' || event.code === 'Space' || event.key === ' ') {
+                        answer.classList.add('is-visible');
+                        answer.classList.remove('is-hidden');
+                        plusIcon.classList.add('is-hidden');
+                        minusIcon.classList.remove('is-hidden');
+                    }
+                }
+                else {
+                    console.error("Minus icon not found.");
                 }
         });
     }
+     else {
+        console.error("Plus icon not found.");
+     }
+
+    if (minusIcon) {
+        if (plusIcon) {
+                minusIcon.addEventListener('click', () => {
+                answer.classList.remove('is-visible');
+                answer.classList.add('is-hidden');
+                plusIcon.classList.remove('is-hidden');
+                minusIcon.classList.add('is-hidden');  
+            });
+        }
+        else {
+            console.error("Plus icon not found.");
+        }
+            minusIcon.addEventListener('keydown', (event) => {
+                if (plusIcon) {
+                    if(event.key === 'Enter' || event.code === 'Space' || event.key === ' ') {
+                        answer.classList.remove('is-visible');
+                        answer.classList.add('is-hidden');
+                        plusIcon.classList.remove('is-hidden');
+                        minusIcon.classList.add('is-hidden');
+                    }
+                }
+                else {
+                    console.error("Plus icon not found.");
+                }
+        });
+    }
+    else {
+        console.error("Minus icon not found.");
+    }
   }
+  
     else {
         console.error("Answer not found.");
 }
